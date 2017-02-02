2ea0bd7b77348c3b2e1b8164ca4ad52ca87a50af
Adam Carpenter <adamcarpenter86@gmail.com>
2017-02-01 21:53:40 -0700

    Number Guesser Master

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ var clearButton = document.getElementById('clear');
 var resetButton = document.getElementById('reset-btn');
 var rangeButton = document.getElementById('userRange');
 var userInput = document.querySelector('.guest-guess');
+var minField = document.querySelector('.min-range');
+var maxField = document.querySelector('.max-range');
 var minNum = 1;
 var maxNum = 100;
-var generatedNum ;
+var generatedNum;
 
 function randomNum(minNum, maxNum) {
   minNum = parseInt(minNum, 10);
@@ -14,6 +16,21 @@ function randomNum(minNum, maxNum) {
   console.log (generatedNum)
 };
 
+function displayGuess(guess) {
+  document.querySelector('.userNum').innerText = guess;
+};
+
+function userResult(guess, generated) {
+  if (guess < generated && guess >= minNum) {
+    document.querySelector('.prevGuess').innerText = "That is too low";
+  } else if (guess > generated && guess <= maxNum) {
+    document.querySelector('.prevGuess').innerText = "That is too high";
+  } else if (guess == generated){
+    document.querySelector('.prevGuess').innerText = "BOOM!";
+  } else if (guess > maxNum | guess < minNum){
+    document.querySelector('.prevGuess').innerText = "Please choose a number within the set range";
+  }
+};
 
 window.onload = function() {
   randomNum (minNum, maxNum);
@@ -24,52 +41,39 @@ rangeButton.addEventListener('click', function(e){
   minNum = document.getElementById('minField').value;
   maxNum = document.getElementById('maxField').value;
   if (minNum > maxNum){
-    alert('Minimum range cannot be less then maximum')
+    alert('Please enter minimum range lower then maximum')
   }
   else {
     randomNum(minNum, maxNum)
   }
 });
 
-
-//guess button
 guessButton.addEventListener("click", function () {
   var userGuesser = userInput.value;
   userResult(userGuesser, generatedNum);
   displayGuess(userGuesser);
   checkInput();
+  disRange();
 });
-
-function displayGuess(guess) {
-  document.querySelector('.userNum').innerText = guess;
-};
 
 clearButton.addEventListener('click', function(){
   document.getElementById('input-field').value = "";
   checkInput();
+  disRange()
 });
 
 resetButton.addEventListener('click', function(){
-    window.location.reload();
+  window.location.reload();
 });
-
-
-function userResult(guess, generated) {
-  if (guess < generated && guess >= minNum) {
-    document.querySelector('.prevGuess').innerText = "That is too low";
-  } else if (guess > generated && guess <= maxNum) {
-    document.querySelector('.prevGuess').innerText = "That is too high";
-  } else if (guess == generated){
-    document.querySelector('.prevGuess').innerText = "BOOM!";
-  } else {
-    document.querySelector('.prevGuess').innerText = "Please choose a number between 1 and 100";
-  }
-};
 
 userInput.addEventListener('keyup', function(){
   clearButton.disabled=false;
   resetButton.disabled=false;
   guessButton.disabled=false;
+});
+
+maxField.addEventListener('keyup', function(){
+  rangeButton.disabled=false;
 });
 
 function checkInput(){
@@ -80,5 +84,9 @@ function checkInput(){
   };
 };
 
-// var generatedNum = Math.floor(Math.random() * (maxNum-minNum + 1))+minNum;
-// console.log(generatedNum, "generatedNum")
+function disRange(){
+  var inputVal = document.getElementById('input-field').value
+  if (inputVal !== "") {
+    rangeButton.disabled=true;
+  };
+};
